34ea128ced5ad25e66477b8406aea9bad83e51e7
codeleaded <gnu.c.reaper@gmail.com>
2026-03-29 16:30:57 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,26 +1,32 @@
 #if defined __linux__
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/Supermarket.h"
 #elif defined _WINE
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/Supermarket.h"
 #elif defined _WIN32
 #include "F:/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "F:/home/codeleaded/System/Static/Library/Supermarket.h"
 #elif defined(__APPLE__)
 #error "Apple not supported!"
 #else
 #error "Platform not supported!"
 #endif
 
+Supermarket market;
+
 void Setup(AlxWindow* w){
-	
+	market = Supermarket_New();
 }
 void Update(AlxWindow* w){
 	
+
 	Clear(BLACK);
 
 	
 }
 void Delete(AlxWindow* w){
-	
+	Supermarket_Free(&market);
 }
 
 int main(){
